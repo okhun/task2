8e6a65e365480a09ea6d52cf930012a4ea6719de
okhun <okhunjongaybullaev@gmail.com>
2022-01-16 21:16:55 +0500

    Webpack configuration

---
+++ src/component/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const App = () => {
+  
+  return <div>It is React Component</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HTmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "built"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HTmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
